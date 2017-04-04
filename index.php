bbe1046d7b37fd62097aa33b678a288e878c2b62
Jesse Humphries <jesseh@hlg.co>
2017-04-04 12:01:48 -0500

    fixing header filters

--- index.php
+++ index.php
@@ -719,7 +719,7 @@
             font-family:{font:Header Filter Font};
             {block:ifboldfiltertext}font-weight:bold;{/block:ifboldfiltertext}
             {block:ifitalicfiltertext}font-style:italic;{/block:ifitalicfiltertext}
-            color:{color:Header Filter Texts Color};
+            color:{color:Header Filter Texts Color} !important;
             border-bottom:1px solid {color:Header Filter Border Color};
         }
         .header_filter a:hover{
